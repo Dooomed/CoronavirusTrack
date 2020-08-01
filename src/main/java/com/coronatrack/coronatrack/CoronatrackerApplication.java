46534ad8151c47c43c5479d671e69c203cab7c62
Doomeedd <yarik030238@gmail.com>
2020-08-01 22:40:08 +0300

    First commit

---
+++ src/main/java/com/coronatrack/coronatrack/CoronatrackerApplication.java
@@ -0,0 +1,15 @@
+package com.coronatrack.coronatrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CoronatrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronatrackerApplication.class, args);
+    }
+
+}
